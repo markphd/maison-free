3e935d68b50794649300c9aeb86d75f350a234f7
Mark Philip Dawal <radical.mark@gmail.com>
2017-06-21 18:17:03 +0400

    using materialize-css

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,39 +1,51 @@
 'use strict';
 
-document.addEventListener('DOMContentLoaded', function() {
-    var slider = document.querySelector('.js_slider');
-    var sliderIndex = document.getElementById('slider-index');
-    var slideCount = document.getElementsByClassName('js_slide').length;
+// document.addEventListener('DOMContentLoaded', function() {
+//     var slider = document.querySelector('.js_slider');
+//     var sliderIndex = document.getElementById('slider-index');
+//     var slideCount = document.getElementsByClassName('js_slide').length;
 
-    var prev = document.getElementsByClassName('js_prev');
-    var next = document.getElementsByClassName('js_next');
+//     var prev = document.getElementsByClassName('js_prev');
+//     var next = document.getElementsByClassName('js_next');
     
-	var index = 1;
+// 	var index = 1;
     
-    function indexCounter(){
- 		console.log(this.className);
-    	this.className === "js_prev prev" ? --index : ++index
-    	if (index == 0) {
-    		index = slideCount;
-    	} else if (index > slideCount){
-    		index = 1;
-    	}
-    	console.log(index);
-    	sliderIndex.innerHTML = index + '<span style="font-style: italic; margin: 0 3px;"> of </span>' + slideCount;
-    }
+//     function indexCounter(){
+//  		console.log(this.className);
+//     	this.className === "js_prev prev" ? --index : ++index
+//     	if (index == 0) {
+//     		index = slideCount;
+//     	} else if (index > slideCount){
+//     		index = 1;
+//     	}
+//     	console.log(index);
+//     	sliderIndex.innerHTML = index + '<span style="font-style: italic; margin: 0 3px;"> of </span>' + slideCount;
+//     }
 
-    prev[0].addEventListener('click', indexCounter);
-    next[0].addEventListener('click', indexCounter);
+//     prev[0].addEventListener('click', indexCounter);
+//     next[0].addEventListener('click', indexCounter);
 
-    if (!index == 0) {
-    	sliderIndex.insertAdjacentHTML('afterbegin', index + '<span style="font-style: italic; margin: 0 3px;"> of </span>' + slideCount);
-    }
+//     if (!index == 0) {
+//     	sliderIndex.insertAdjacentHTML('afterbegin', index + '<span style="font-style: italic; margin: 0 3px;"> of </span>' + slideCount);
+//     }
 
 
 
-    console.log(slideCount);
+//     console.log(slideCount);
 
-    lory(slider, {
-        infinite: 1
+//     lory(slider, {
+//         infinite: 1
+//     });
+// });
+
+  $(document).ready(function(){
+      $('.slider').slider({
+        "indicators": false
+      });
+      $('.js_prev').click(function() {
+          $('.slider').slider('prev');
+      });
+      $('.js_next').click(function() {
+          $('.slider').slider('next');
+      });
     });
-});
